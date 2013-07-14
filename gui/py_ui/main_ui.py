245467a088e09fcbf866e6e7928e7851d6f5c2cc
Bartosz Stalewski <bartekstalewski@gmail.com>
2013-07-14 18:03:42 +0200

    Updated ui creation script

--- gui/py_ui/main_ui.py
+++ gui/py_ui/main_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/main.ui'
 #
-# Created: Sun Jul 14 10:10:20 2013
+# Created: Sun Jul 14 18:03:07 2013
 #      by: PyQt4 UI code generator 4.10.2-snapshot-74ade0e1faf2
 #
 # WARNING! All changes made in this file will be lost!
